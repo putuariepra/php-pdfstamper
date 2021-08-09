d6a5d297698b38d06d4e3356f3e421ce1992e30f
Arie Pratama <putuariepratama@gmail.com>
2021-08-09 14:10:41 +0800

    Fixed bug on Windows: Current Working Dir Mode

--- src/PdfStamper.php
+++ src/PdfStamper.php
@@ -5,8 +5,14 @@ class PdfStamper {
   private $dirLib;
   private $fileName;
   private $fileExt;
+  private $fileDir;
 
-  private $validate = 0;
+  /**
+   * 0: Default
+   * 1: Current Working Dir Mode (CWD). Copy file to current working dir and use the file to stamping
+   */
+  private $mode = 0;
+  private $validate = 1;
 
   protected $targetPdf = null;
   protected $outpurDir = null;
@@ -36,12 +42,13 @@ class PdfStamper {
     'svgz' => 'image/svg+xml',
   ];
 
-  function __construct($targetPdf, $imagePath, $outpurDir) {
+  function __construct($targetPdf, $imagePath, $outpurDir=null) {
     $this->dirLib = \dirname(__FILE__);
 
     $fileinfo = \pathinfo($targetPdf);    
     $this->fileName = $fileinfo['filename'];
     $this->fileExt = $fileinfo['extension'];
+    $this->fileDir = $fileinfo['dirname'];
 
     $this->targetPdf = $targetPdf;
     $this->imagePath = $imagePath;
@@ -49,6 +56,14 @@ class PdfStamper {
   }  
 
   public function render() {
+    $imagePath = $this->imagePath;
+    $targetPdf = $this->targetPdf;
+    $outpurDir = $this->outpurDir;
+
+    if (is_null($outpurDir)) {
+      $outpurDir = $this->fileDir;
+    }
+
     if ($this->validate) {      
       if (! \is_readable($this->targetPdf)) {
         return $this->getOutput(false, 'PDF is not found nor readable');
@@ -56,8 +71,8 @@ class PdfStamper {
       if (! \is_readable($this->imagePath)) {
         return $this->getOutput(false, 'Image is not found nor readable');
       }
-      if (! \is_writable($this->outpurDir)) {
-        return $this->getOutput(false, 'Output folder is not found nor writeable');
+      if (! \is_writable($outpurDir)) {
+        return $this->getOutput(false, "Output folder {$outpurDir} is not found nor writeable");
       }
       if (\mime_content_type($this->targetPdf) != 'application/pdf') {
         return $this->getOutput(false, 'Supported file format is only PDF');
@@ -77,21 +92,46 @@ class PdfStamper {
       $stampUrl = ' -u '.$this->stampUrl;
     }
 
-    $outputPath = $this->getOutputPath();
-    if (\file_exists($outputPath)) {            
-      if ($this->overwrite) {
-        \unlink($outputPath);
-      }else{
-        return $this->getOutput(false, 'Stamped file already exists');
-      }
+    $outputCmd = '';
+    if (!is_null($this->outpurDir)) {
+      $outputCmd = ' -o '.$this->outpurDir;
     }
 
-    $command = "java -jar {$this->dirLib}/pdfstamp.jar{$dpi}{$stampUrl} -i '{$this->imagePath}' -l {$this->locX},{$this->locY} '{$this->targetPdf}' 2>&1";    
+    $outputPath = $this->getOutputPath($outpurDir);
+    if (\file_exists($outputPath) && !$this->overwrite) {                  
+      return $this->getOutput(false, 'Stamped file already exists');      
+    }
+
+    /**
+     * Copy file to current working dir and use the file to stamping
+     */
+    if ($this->mode == 1) {
+      $cwd = \getcwd();
+      $new_file = $cwd.'/'.$this->fileName;
+      $image_filename = \basename($this->imagePath);
+      $new_image = $cwd.'/'.$image_filename;
+      if (!copy($this->targetPdf,$new_file)) {
+        return $this->getOutput(false, "Failed to copy file {$this->fileName} to {$cwd}");
+      }      
+      if (!copy($this->imagePath,$new_image)) {
+        return $this->getOutput(false, "Failed to copy file {$this->fileName} to {$cwd}");
+      }
+
+      $imagePath = $image_filename;
+      $targetPdf = $this->fileName;
+    }
+
+    $command = "java -jar {$this->dirLib}/pdfstamp.jar{$dpi}{$stampUrl}{$outputCmd} -i '{$imagePath}' -l {$this->locX},{$this->locY} '{$targetPdf}' 2>&1";
 
     try {    
       \exec($command, $val, $err);
     } catch (\Exception $e) {      
       return $this->getOutput(false, $e->getMessage());
+    }
+
+    if ($this->mode == 1) {
+      \unlink($new_file);
+      \unlink($new_image);
     }
 
     if (!empty($val)) {      
@@ -125,16 +165,21 @@ class PdfStamper {
     return $this;
   }
 
-  static public function stamp($targetPdf, $imagePath, $outpurDir) {
+  public function modeCwd() {
+    $this->mode = 1;
+    return $this;
+  }
+
+  static public function stamp($targetPdf, $imagePath, $outpurDir=null) {
     return new self($targetPdf, $imagePath, $outpurDir);
   }
 
-  protected function getOutputPath() {    
-    if (substr($this->outpurDir, -1, 1) != '/') {
-      return "{$this->outpurDir}/{$this->fileName}_stamped.{$this->fileExt}";
+  protected function getOutputPath($outpurDir) {
+    if (substr($outpurDir, -1, 1) != '/') {
+      return "{$outpurDir}/{$this->fileName}_stamped.{$this->fileExt}";
     }
 
-    return "{$this->outpurDir}{$this->fileName}_stamped.{$this->fileExt}";
+    return "{$outpurDir}{$this->fileName}_stamped.{$this->fileExt}";
   }
 
   protected function getOutput($success, $msg, $outputPath=null) {
